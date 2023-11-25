2d4775bbec37635afc2023d8b1cf6a91aae245f7
praveennarni <praveenbabunarni1999@gmail.com>
2023-11-25 12:05:49 -0600

    Added Spring Project

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ * @author
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
